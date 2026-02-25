bada6a937ac42a67ffdbaa53e98187fd2a23c7ac
Tomas Vanagas <tomas.vanagas@knf.vu.lt>
2026-02-25 18:26:30 +0200

    Push via filebrowser version tag

--- pushDockerhub.sh
+++ pushDockerhub.sh
@@ -2,11 +2,11 @@
 set -e
 
 CONTAINER_NAME=filebrowser-noauth
+VERSION=v2.54.0
 
-TODAY=$(date +%Y%m%d)
 sudo docker login -u admin@knf.vu.lt
 sudo docker build -t $CONTAINER_NAME .
 sudo docker tag $CONTAINER_NAME vuknf/$CONTAINER_NAME:latest
-sudo docker tag $CONTAINER_NAME vuknf/$CONTAINER_NAME:$TODAY
+sudo docker tag $CONTAINER_NAME vuknf/$CONTAINER_NAME:$VERSION
 sudo docker push vuknf/$CONTAINER_NAME:latest
-sudo docker push vuknf/$CONTAINER_NAME:$TODAY
+sudo docker push vuknf/$CONTAINER_NAME:$VERSION
